9c246eaca9db83fabe7427d74d321a3450cce73e
outsiderintrospector <juansebcarv@outlook.com>
2022-02-12 16:35:22 -0300

    update

--- pages/works/walknote.js
+++ pages/works/walknote.js
@@ -100,7 +100,7 @@ const Work = () => (
           width="640"
           height="360"
           frameBorder="0"
-          allowFullscreen
+          allowFullScreen
         />
       </AspectRatio>
       {/* SLIDER */}
